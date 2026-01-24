4f12e843d34cca0db095d0829f38fec683d0899c
Imomazin <imomazin.ae@gmail.com>
2026-01-24 20:04:24 +0000

    Add homepage using pages router

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Strathmore Cleaning</h1>
+      <p>Professional cleaning services in the UK.</p>
+    </main>
+  );
+}
